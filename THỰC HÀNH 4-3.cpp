6610fd1947ba138bf24282c64a8a01fcc1fe3726
thanhtainguyen <taintuitktmt@gmail.com>
2023-11-05 15:42:47 +0700

    codebasic

---
+++ THỰC HÀNH 4-3.cpp
@@ -0,0 +1,26 @@
+﻿//Nhập vào số năm
+//In ra kết luận năm đó là năm nhuận hay năm thường.Năm nhuận là :
+//năm chia hết cho 400, hoặc
+//năm chia hêt cho 4 và ko chia hết cho 100
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int year;
+	cout << "Nhap nam: "; cin >> year;
+
+	if (year % 400 == 0)
+	{
+		cout << "Nam " << year << " la nam nhuan";
+	}
+	else if (year % 4 == 0 && year % 100 != 0)
+	{
+		cout << "Nam " << year << " la nam nhuan";
+	}
+	else
+	{
+		cout <<"Nam " << year << " khong phai la nam nhuan (nam thuong)";
+	}
+}
